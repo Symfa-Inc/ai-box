cc7bffa8709868ffd1bb61abaf61538cdedd198f
Ilya Mokin <ilia.mokin@gmail.com>
2024-01-21 02:37:50 +0100

    init script update

--- docker_init.py
+++ docker_init.py
@@ -6,8 +6,8 @@ from pyannote.audio import Pipeline
 
 read_key = os.environ.get('HF_TOKEN', None)
 print(f"HF_TOKEN: {read_key}")
-pyannote_pipeline = Pipeline.from_pretrained('pyannote/speaker-diarization', use_auth_token=read_key)
-
+pyannote_pipeline = Pipeline.from_pretrained('pyannote/speaker-diarization-3.1', use_auth_token=read_key)
+pyannote_pipeline.to(torch.device("cuda"))
 
 w_model_id = "openai/whisper-large-v3"
 
