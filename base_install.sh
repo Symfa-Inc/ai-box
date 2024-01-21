a48228f8361e4f71900d5ec5fbfdddc7a3ba2d1d
Ilya Mokin <ilia.mokin@gmail.com>
2024-01-21 01:31:48 +0000

    script for fast  machine init

--- base_install.sh
+++ base_install.sh
@@ -1,6 +1,6 @@
 add-apt-repository ppa:deadsnakes/ppa -y
 apt update -y
-apt install python3.10-dev -python3 -y
+apt install python3.10-dev -y
 apt install python3.10-distutils -y
 apt install ffmpeg libavcodec-extra -y
 apt install libpq-dev -y
